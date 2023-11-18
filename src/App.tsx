f32dcbfe53f12cf3b128a9405def440d030ebd23
Pablo Silva <pablolucio_@hotmail.com>
2023-11-18 07:32:03 -0300

    styles: created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
-
-interface ILesson {
-  id: string;
-  title: string;
-}
+import { Event } from './pages/Event'
 
 function App() {
 
-  const GET_LESSONS_QUERY = gql`
-    query{
-      lessons{
-        id
-        title
-      },
-      teachers{
-        id
-        name
-      }
-    }
-  `
-  const { data } = useQuery<{lessons: ILesson[]}>(GET_LESSONS_QUERY)
-
   return (
     <>
-      <ul>
-        {data?.lessons.map(item => (
-          <li>{item.title}</li>
-        ))}
-      </ul>
+      <Event />
     </>
   )
 }
